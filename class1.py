235f7f34fa099388d50db119dc4cc87d306971e2
Neeraj Kumar <nkurmbanshi@yahoo.com>
2023-06-12 17:48:52 -0400

    First Commit

---
+++ class1.py
@@ -0,0 +1,15 @@
+class Test :
+    @staticmethod
+    def static_method_1():
+        print('static method 1')
+
+    @staticmethod
+    def static_method_2() :
+        Test.static_method_1()
+
+    @classmethod
+    def class_method_1(cls) :
+        cls.static_method_2()
+
+# call class method
+Test.class_method_1()
---
+++ static3.py
@@ -0,0 +1,21 @@
+class Employee:
+    def __init__(self,name,age,salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+    
+    @staticmethod
+    def incremented_salary(salary):
+        salary = salary + 12000
+        return("Incremented Salary is: ", salary)
+    
+    def display(self):
+        print("Name: ", self.name)
+        print("Age: ", self.age)
+        print("Salary: ", self.salary)
+        print("Incremented Salary is: ", Employee.incremented_salary(self.salary))
+        
+    
+        
+obj = Employee("Neeraj",35,25000)
+obj.display()
